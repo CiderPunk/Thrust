f740b9c99847824e7127d554e6e785ec999070c5
Matt Lander <matt@ciderpunk.net>
2026-02-10 20:17:41 +0000

    camera movement

--- src/camera.rs
+++ src/camera.rs
@@ -1,3 +1,4 @@
+use avian3d::prelude::LinearVelocity;
 use bevy::prelude::*;
 
 use crate::{game_schedule::GameSchedule, game_state::GameState, player::Player};
@@ -36,10 +37,23 @@ fn remove_scene_cameras(query:Query<&mut Camera, Without<GameCamera>> ){
   }
 }
 
+
+const LOOK_AHEAD_TIME:f32 = 0.5;
+const MIN_LOOK_AHEAD_VELOCITY:f32 = 4.;
+
 fn update_camera(
-  player:Single<&GlobalTransform, With<Player>>,
+  player:Single<(&GlobalTransform, &LinearVelocity), With<Player>>,
   mut camera:Single<&mut Transform, With<GameCamera>>,
+  time:Res<Time>,
 ){
-  camera.translation = Vec3::new(player.translation().x, player.translation().y, player.translation().z + 100.);
+  let (transform, velocity) = *player;
+
+  let view_position = Vec3::new(transform.translation().x, transform.translation().y, transform.translation().z + 100.) 
+    - if velocity.0.length_squared() > MIN_LOOK_AHEAD_VELOCITY * MIN_LOOK_AHEAD_VELOCITY { (velocity.0 * LOOK_AHEAD_TIME) } else{ (Vec3::ZERO)};
+  camera.translation = camera.translation.lerp(view_position, time.delta_secs() * 2.0);
+  camera.look_at(transform.translation(), Vec3::Y);
+
+
+  //camera.translation = Vec3::new(player.translation().x, player.translation().y, player.translation().z + 100.);
 
 }
