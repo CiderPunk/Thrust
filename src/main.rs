aa67e7701dd02dbe99f3efe0ca2cb1018dd2be02
Matt Lander <matt@ciderpunk.net>
2026-02-11 00:27:57 +0000

    app name and window size so it might go fullscreen

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,36 @@ mod game_schedule;
 mod camera;
 
 
-use bevy::{color::palettes::css::WHITE, prelude::*};
+use bevy::{asset::AssetMetaCheck, color::palettes::css::WHITE, prelude::*};
 use bevy_enhanced_input::EnhancedInputPlugin;
 use bevy_skein::SkeinPlugin;
 use avian3d::prelude::*;
 
 use crate::{asset_management::AssetManagementPlugin, camera::CameraPlugin, game::GamePlugin, game_schedule::GameSchedulePlugin, game_state::GameStatePlugin, map::MapPlugin, player::PlayerPlugin};
 
+
+const APP_NAME: &str = "Caves";
+
 fn main() {
   App::new()
+    .add_plugins(
+      DefaultPlugins.set(WindowPlugin {
+          primary_window: Some(Window {
+            title: APP_NAME.into(),
+            name: Some(APP_NAME.into()),
+            fit_canvas_to_parent: true,
+            visible: true,
+            ..default()
+          }),
+          ..default()
+        })
+         .set(AssetPlugin {
+          meta_check: AssetMetaCheck::Never,
+          watch_for_changes_override: Some(true),
+          ..default()
+        }),
+      )
     .add_plugins((
-      DefaultPlugins, 
       SkeinPlugin::default(), 
       PhysicsPlugins::default(),
       EnhancedInputPlugin,
