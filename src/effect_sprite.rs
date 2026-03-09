1eb017743337cd30deb57d5f87cdb15bc8f4d160
Matt Lander <matt@ciderpunk.net>
2026-03-09 00:53:19 +0000

    very work in progress effect sprite loading

--- src/effect_sprite.rs
+++ src/effect_sprite.rs
@@ -1,5 +1,5 @@
 use bevy::{
-  asset::RenderAssetUsages, math::VectorSpace, mesh::{Indices, MeshTag, PrimitiveTopology}, prelude::*, render::render_resource::{AsBindGroup, BufferUsages, BufferVec, ShaderType}, shader::ShaderRef
+  mesh::MeshTag, platform::collections::HashMap, prelude::*, render::render_resource::{AsBindGroup, ShaderType}, shader::ShaderRef
 };
 use bevy_asset_loader::prelude::*;
 use bevy_common_assets::json::JsonAssetPlugin;
@@ -9,11 +9,22 @@ const MAX_EFFECT_FRAMES:usize = 50;
 
 const EFFECT_SPRITE_SHADER_PATH: &str = "shaders/animated_uv.wgsl";
 
+const EFFECT_TYPE_SPLOSION:&'static str = "splosion";
+
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub enum EffectSpriteType {
+  Splosion,
+}
+
+
 pub struct EffectSpritePlugin;
 
 impl Plugin for EffectSpritePlugin{
   fn build(&self, app: &mut App) {
     app
+      
+      .init_resource::<EffectDefinitionCollection>()
       .init_resource::<EffectMaterials>()
       .add_plugins(JsonAssetPlugin::<SpriteMapData>::new(&["json"]))
       .add_plugins(MaterialPlugin::<EffectSpriteMaterial>::default())
@@ -22,27 +33,78 @@ impl Plugin for EffectSpritePlugin{
           .load_collection::<SpriteSheetAssets>(),
       )
       .add_message::<EffectSpriteMessage>()
+      .add_systems(Startup, (init_effect_definitions, init_mesh))
+      .add_systems(OnEnter(AssetLoadState::Startup), load_sprite_sheets)
       .add_systems(OnEnter(AssetLoadState::Loaded), init_sprite_sheets)
-      .add_systems(Startup, init_mesh)      
       .add_systems(OnEnter(GameState::Initialize), spawn_effect_sprites);
   }
 }
 
+#[derive(Resource)]
+struct EffectSpriteDefinition {
+  effect_type: &'static str,
+  path: &'static str,
+  frame_rate: f32,
+}
+
+#[derive(Resource, Default)]
+struct EffectDefinitionCollection(Vec<EffectSpriteDefinition>);
+
+
+fn init_effect_definitions(
+  mut effects:ResMut<EffectDefinitionCollection>,
+){
+  effects.0.push(
+   EffectSpriteDefinition {
+    effect_type: EFFECT_TYPE_SPLOSION,
+    path: "effect_sprites/splosion1",
+    frame_rate:30.
+  });
+  effects.0.push(
+   EffectSpriteDefinition {
+    effect_type: EFFECT_TYPE_SPLOSION,
+    path: "effect_sprites/splosion2",
+    frame_rate:30.
+  });
+  effects.0.push(
+   EffectSpriteDefinition {
+    effect_type: EFFECT_TYPE_SPLOSION,
+    path: "effect_sprites/splosion3",
+    frame_rate:30.
+  });
+}
+
+fn load_sprite_sheets(
+  effect_defs:Res<EffectDefinitionCollection>,
+  mut dynamic_assets: ResMut<DynamicAssets>,
+){ 
+  let image_paths = effect_defs.0.iter().map(|f| format!("{}.png", f.path)).collect();
+  let map_paths = effect_defs.0.iter().map(|f| format!("{}.json", f.path)).collect();
+  dynamic_assets.register_asset(
+    "spritesheet_images",
+    Box::new(StandardDynamicAsset::Files{ paths: image_paths })
+  );
+  dynamic_assets.register_asset(
+    "spritesheet_maps",
+    Box::new(StandardDynamicAsset::Files{ paths: map_paths })
+  );
+}
 
 #[derive(Resource)]
 struct EffectQuad(Handle<Mesh>);
 
-
 #[derive(Message)]
 pub struct EffectSpriteMessage {
+  effect_type: String,
   translation: Vec3,
   scale: f32,
   velocity: Vec3,
 }
 
 impl EffectSpriteMessage {
-  pub fn new(translation: Vec3, scale: f32, velocity: Vec3) -> Self {
+  pub fn new(effect_type:String, translation: Vec3, scale: f32, velocity: Vec3) -> Self {
     Self {
+      effect_type,
       translation,
       scale,
       velocity,
@@ -52,19 +114,24 @@ impl EffectSpriteMessage {
 
 
 fn spawn_effect_sprites(
+  mut reader:MessageReader<EffectSpriteMessage>,
   mut commands: Commands,
   mesh: Res<EffectQuad>,
   effects: Res<EffectMaterials>,
   time: Res<Time>,
 ) {
-      let offset: f32 = time.elapsed_secs_wrapped();
-info!("spawning effect");
-  commands.spawn((
-    Mesh3d(mesh.0.clone()),
-    MeshMaterial3d(effects.splosion.clone()),
-    Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(10.)),
-    MeshTag(offset.to_bits()),
-  ));
+  let offset: u32 = time.elapsed_secs_wrapped().to_bits();
+  for effect_message in reader.read(){
+    info!("spawning effect");
+    commands.spawn((
+      Mesh3d(mesh.0.clone()),
+    //  MeshMaterial3d(effects.splosion.clone()),
+      Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(10.)),
+      MeshTag(offset),
+    ));
+
+    }  
+    
 }
 
 
@@ -74,47 +141,35 @@ fn init_mesh(
   mut commands:Commands,
 ){
   //let quad = meshes.add(create_quad());
-
-
    let quad =  meshes.add(Plane3d::new(Vec3::Z, Vec2::new(1.,1.)));
   //let quad = meshes.add(Sphere::new(2.).mesh().uv(32, 18));
   commands.insert_resource(EffectQuad(quad));
 }
 
-/*
-fn create_quad() -> Mesh {
-  Mesh::new(PrimitiveTopology::TriangleList,
-    RenderAssetUsages::default(),
-  )
-  .with_inserted_attribute(
-    Mesh::ATTRIBUTE_POSITION,
-    vec![[-1., -1., 0.], [1., -1., 0.], [1., 1., 0.], [-1., 1., 0.]],
-  )
-  .with_inserted_attribute(
-    Mesh::ATTRIBUTE_UV_0,
-    vec![[0., 0.], [1., 0.], [1., 1.], [0., 1.]],
-  )
-  .with_inserted_attribute(
-    Mesh::ATTRIBUTE_NORMAL,
-    vec![
-      [0., 0., -1.],
-      [0., 0., -1.],
-      [0., 0., -1.],
-      [0., 0., -1.],
-    ],
-  )
-  .with_inserted_indices(Indices::U32(vec![0, 2, 1, 0, 3, 2]))
-}
- */
-
 fn init_sprite_sheets(
-  sprite_sheets:Res<SpriteSheetAssets>,
+  effect_defs:Res<EffectDefinitionCollection>,
+  sprite_resources:Res<SpriteSheetAssets>,
   sprite_map_assets: Res<Assets<SpriteMapData>>,
-  mut effect_materials:ResMut<EffectMaterials>,
   mut materials: ResMut<Assets<EffectSpriteMaterial>>,
-
+  mut effect_materials:ResMut<EffectMaterials>,
 ){
-  let Some(sprite_map) = sprite_map_assets.get(sprite_sheets.splosion_map.id()) else {
+  for (i,effect_def) in effect_defs.0.iter().enumerate(){
+    let material = init_sprite_sheet(sprite_resources.maps[i].clone(), sprite_resources.images[i].clone(), effect_def.frame_rate, sprite_map_assets, materials);
+
+
+  }
+
+}
+
+
+fn init_sprite_sheet(
+  sprite_sheet_map:Handle<SpriteMapData>,
+  sprite_sheet_image:Handle<Image>,
+  frame_rate:f32,
+  sprite_map_assets: &Res<Assets<SpriteMapData>>,
+  mut materials: ResMut<Assets<EffectSpriteMaterial>>,
+) -> Handle<EffectSpriteMaterial>{
+  let Some(sprite_map) = sprite_map_assets.get(sprite_sheet_map.id()) else {
     panic!("Failed loading sprite map");
   };
   //sort the frames by filename frame number!
@@ -134,7 +189,6 @@ fn init_sprite_sheets(
   let mut frame_defs = [FrameDefinition { uv_rect: Vec4::ZERO, trim_rect: Vec4::ZERO }; MAX_EFFECT_FRAMES];
 
   for (i,(_, frame)) in sortable.iter().enumerate(){
-
     let tile_size = Vec2{ x: frame.source_size.w as f32, y: frame.source_size.h as f32 };
     let uv_rect = Vec4::new(
       frame.frame.x as f32 / canvas_size.x, 
@@ -148,30 +202,25 @@ fn init_sprite_sheets(
       (((frame.sprite_source_size.x + frame.sprite_source_size.w) as f32 / tile_size.x)- 0.5)  * 2.,
       (((frame.sprite_source_size.y + frame.sprite_source_size.h) as f32 / tile_size.y)- 0.5)  * 2.,
     );
-
-    info!("trim: {}", trim_rect);
     frame_defs[i] = FrameDefinition { uv_rect, trim_rect };
   }
 
+  
   let material = materials.add(EffectSpriteMaterial{ 
     settings: EffectSpriteSettings { 
-      frame_rate: 25., 
+      frame_rate: frame_rate, 
       frame_count: frame_count as u32, 
       filler: Vec2::ZERO,
     }, 
     frames: frame_defs, 
-    texture_atlas: sprite_sheets.splosion_image.clone(), 
+    texture_atlas: sprite_sheet_image.clone(), 
     alpha_mode: AlphaMode::Premultiplied
    });
-  effect_materials.splosion = material;
-
+   (material)
 }
 
 #[derive(Resource, Default)]
-pub struct EffectMaterials{
-  pub splosion:Handle<EffectSpriteMaterial>,
-}
-
+pub struct EffectMaterials(HashMap<String, Vec<EffectSpriteMaterial>>);
 
 
 #[derive(Default, Clone, Copy, AsBindGroup, Debug, ShaderType)]
@@ -186,7 +235,6 @@ pub struct FrameDefinition{
   uv_rect:Vec4,
   trim_rect:Vec4,
 }
-
 
 #[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
 pub struct EffectSpriteMaterial {
@@ -213,16 +261,14 @@ impl Material for EffectSpriteMaterial {
   }
 }
 
-
-
-
 #[derive(AssetCollection, Resource)]
 pub struct SpriteSheetAssets{
-  #[asset(paths("effect_sprites/splosion.png"), collection(typed))]
+  #[asset(key="spritesheet_images", collection(typed))]
   images: Vec<Handle<Image>>,
-  #[asset(path="effect_sprites/splosion.json")]
-  pub splosion_map:Handle<SpriteMapData>,
+  #[asset(key="spritesheet_maps", collection(typed))]
+  maps:Vec<Handle<SpriteMapData>>,
 }
+
 
 // region: sprite map
 
